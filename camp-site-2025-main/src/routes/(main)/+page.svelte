9e8f507d36e316a21916fd5c36578ef9daa06994
ntnucsietech <ntnu.csie.tech@gmail.com>
2026-04-13 21:08:16 +0800

    Redirect to updated index.html for 2026

--- camp-site-2025-main/src/routes/(main)/+page.svelte
+++ camp-site-2025-main/src/routes/(main)/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import { browser } from '$app/environment';
     if (browser) {
-        window.location.href = '/2026/CSIE_Camp2026.html';
+        window.location.href = '/2026/index.html';
     }
 </script>

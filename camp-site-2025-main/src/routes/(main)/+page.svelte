fb4a5dd53d30ac27ade9924120fbb18f1d08dc6c
ntnucsietech <ntnu.csie.tech@gmail.com>
2026-04-18 11:40:21 +0800

    Redirect to index.html instead of 2026/index.html

--- camp-site-2025-main/src/routes/(main)/+page.svelte
+++ camp-site-2025-main/src/routes/(main)/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import { browser } from '$app/environment';
     if (browser) {
-        window.location.href = '/2026/index.html';
+        window.location.href = '/index.html';
     }
 </script>
